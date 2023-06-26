e0ed770c938eafd35a5987e9e60bde874e2be54e
Paul Cruz <paulcruz74@gmail.com>
2023-06-26 18:17:03 -0500

    add installation of fonts

--- install.sh
+++ install.sh
@@ -7,6 +7,15 @@ ln -sf $path/.p10k.zsh $HOME/.p10k.zsh
 # Ensure config directory exists
 [ ! -d $HOME/.config ] && mkdir $HOME/.config
 
+# Ensure local share directory exists
+# Local directory
+[ ! -d $HOME/.local ] && mkdir -p $HOME/.local/share
+[ ! -d $HOME/.local/share ] && mkdir $HOME/.local/share
+
+# Fonts
+[ -d $HOME/.local/share/fonts ] && rm -r $HOME/.local/share/fonts
+ln -sf $path/.local/share/fonts $HOME/.local/share/fonts
+
 # Alacritty
 [ -d $HOME/.config/alacritty ] && rm -rf $HOME/.config/alacritty
 ln -sf $path/config/alacritty $HOME/.config/alacritty
