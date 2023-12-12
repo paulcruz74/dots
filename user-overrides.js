5520efad729900f3bd0a89fb42da5fe1eac91e8e
Paul Cruz <paulcruz74@gmail.com>
2023-12-11 21:13:59 -0600

    Add user-overrides.js

---
+++ user-overrides.js
@@ -0,0 +1,12 @@
+//DNS over HTTPS
+// Cloudflare is the default, so we leave the below line out.
+// user_pref("network.trr.uri", "https://mozilla.cloudflare-dns.com/dns-query");
+user_pref("network.trr.mode", 2);
+
+//Firefox stores passwords in plain text and obsolete if you use a password manager.
+//Mozilla also told people to stop using their password manager.
+user_pref("signon.rememberSignons", false);
+
+
+// Re-enable safebrowsing
+user_pref("browser.safebrowsing.downloads.remote.enabled", true);
