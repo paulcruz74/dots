c13c6d639a7fb666ff282ba3249a4d15e83c2e01
Paul Cruz <paul@usepylon.com>
2026-01-24 16:37:30 -0800

    add keyboard repeat rate

--- macos.sh
+++ macos.sh
@@ -1,3 +1,7 @@
 # Reduce dock animation time
 defaults write com.apple.dock autohide-time-modifier -float 0.1
 defaults write com.apple.dock autohide-delay -float 0
+
+# Increase keyboard repeat rate
+defaults write -g KeyRepeat -int 2
+defaults write -g InitialKeyRepeat -int 15
