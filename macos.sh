0b8a87ef88d360ec888692cb056edf66a275c869
Paul Cruz <paul@usepylon.com>
2026-01-24 16:26:13 -0800

    make macos.sh executable and reduce animation time

--- macos.sh
+++ macos.sh
@@ -1,3 +1,3 @@
 # Reduce dock animation time
-defaults write com.apple.dock autohide-time-modifier -float 0.12
+defaults write com.apple.dock autohide-time-modifier -float 0.1
 defaults write com.apple.dock autohide-delay -float 0
