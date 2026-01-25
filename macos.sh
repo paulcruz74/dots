0ce57154e65ac415c6e035209d968b0ee439fa04
Paul Cruz <paul@usepylon.com>
2026-01-24 16:47:22 -0800

    show filename extensions

--- macos.sh
+++ macos.sh
@@ -13,3 +13,6 @@ defaults write com.apple.screencapture location -string "${HOME}/Pictures/screen
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
