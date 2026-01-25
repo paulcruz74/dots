881c0ec4905613758e905ee1a8a58f8243fcbd27
Paul Cruz <paul@usepylon.com>
2026-01-24 16:45:07 -0800

    update screenshots

--- macos.sh
+++ macos.sh
@@ -6,3 +6,10 @@ defaults write com.apple.dock autohide-delay -float 0
 defaults write -g KeyRepeat -int 2
 defaults write -g InitialKeyRepeat -int 15
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Save screenshots to the desktop
+mkdir -p $HOME/Pictures/screenshots
+defaults write com.apple.screencapture location -string "${HOME}/Pictures/screenshots"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
